69b8fdf88b54c94f0af73d4950c8d862df3be372
tu1014 <tu1014@naver.com>
2023-01-17 20:13:17 +0900

    debug complete

--- boot/Main.c
+++ boot/Main.c
@@ -6,4 +6,7 @@ void main(void) {
     uint32_t* dummyAddr = (uint32_t*)(1024*1024*100);
     *dummyAddr = sizeof(long);
 
+    dummyAddr = (uint32_t*)(1024*1024*100 + 4);
+    *dummyAddr = (uint32_t)1;
+
 }
