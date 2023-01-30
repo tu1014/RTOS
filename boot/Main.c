dbbda7a7bd4c84f6d00eacf4c0c1067485bbcaa9
tu1014 <tu1014@naver.com>
2023-01-30 21:41:11 +0900

    implement Timer

--- boot/Main.c
+++ boot/Main.c
@@ -2,10 +2,14 @@
 #include "stdint.h"
 #include "HalUart.h"
 #include "HalInterrupt.h"
+#include "HalTimer.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include "stdbool.h"
 
 static void Hw_init(void);
 static void Printf_test(void);
+static void Timer_test(void);
 
 void main(void) {
 
@@ -15,13 +19,13 @@ void main(void) {
     Printf_test();
     putstr("Finish!\n");
 
-    while(1);
-
+    Timer_test();
 }
 
 static void Hw_init(void) {
     Hal_interrupt_init();
     Hal_uart_init();
+    Hal_timer_init();
 }
 
 static void Printf_test(void)
@@ -29,6 +33,7 @@ static void Printf_test(void)
     char* str = "printf pointer test";
     char* nullptr = 0;
     uint32_t i = 5;
+    uint32_t* sysctrl0 = (uint32_t) 0x10001000;
 
     debug_printf("%s\n", "Hello printf");
     debug_printf("output string pointer: %s\n", str);
@@ -36,4 +41,12 @@ static void Printf_test(void)
     debug_printf("%u = 5\n", i);
     debug_printf("dec=%u hex=%x\n", 0xff, 0xff);
     debug_printf("print zero %u\n", 0);
+    debug_printf("SYSCTRL0 %x\n", *sysctrl0);
+}
+
+static void Timer_test(void) {
+    while(true) {
+        debug_printf("current count : %u\n", Hal_timer_get_1ms_counter());
+        delay(1000);
+    }
 }
