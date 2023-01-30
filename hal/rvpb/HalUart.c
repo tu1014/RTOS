fc7e88100195ff288b7412c6e7159cda23335cf6
tu1014 <tu1014@naver.com>
2023-01-30 10:51:38 +0900

    implement HAL UART

--- hal/rvpb/HalUart.c
+++ hal/rvpb/HalUart.c
@@ -15,3 +15,22 @@ void Hal_uart_put_char(uint8_t ch) {
     while(Uart->uartfr.bits.TXFF); // wait until output buffer is empty
     Uart->uartdr.all = (ch & 0xFF);
 }
+
+uint8_t Hal_uart_get_char(void) {
+
+    uint8_t data;
+
+    while(Uart->uartfr.bits.RXFE);
+
+    data = Uart->uartdr.all;
+
+    // check 4 error flag
+    if (data & 0xFFFFFF00) {
+
+        // clear all error flag
+        Uart->uartdr.all = 0xFF;
+        return 0;
+    }
+
+    return (uint8_t)(data & 0xFF);
+}
